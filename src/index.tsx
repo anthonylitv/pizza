5138fbb89c562c3ae35b9a2602007e55f2c7863d
Anton <toxalitvinenko@gmail.com>
2022-12-21 14:20:09 +0200

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import "./index.scss"
 import App from "./App"
 import { Provider } from "react-redux"
 import store from "./store/index"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
 )
